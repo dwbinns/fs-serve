0b850a6ac8a47566bd4403c08aa10c8e7b4613e7
David Binns <dbinns.web+git@gmail.com>
2020-12-13 16:17:45 +0000

    Hashbang

--- cli.js
+++ cli.js
@@ -1,13 +1,14 @@
+#!/usr/bin/env node
 const {createServer} = require("http");
 const Server = require(".");
 
 (async(port = 4000, directory = ".") => {
     let server = new Server(directory);
-    
+
     createServer((req, res) => {
         server.serve(req, res);
     }).listen(port);
 
     console.log(`Server started http://localhost:${port}/`);
-    
+
 })(...process.argv.slice(2));
